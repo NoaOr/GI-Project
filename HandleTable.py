dac974769809f455d24777853fb21a72a20167c2
Noa Or <noaor7@gmail.com>
2018-11-18 19:42:33 +0200

    fix writing to table - need to run again

--- HandleTable.py
+++ HandleTable.py
@@ -3,9 +3,12 @@ from MetchChecker import *
 
 def add_sentence_to_df_by_match(sentence, accuracy, df, col_name):
 
-    df['acc'] = ""
-    df['match-sent'] = ""
+    if not 'acc' in df.columns:
+        df['acc'] = ""
+        df['match-sent'] = ""
     top_dict = get_top_matches(df, accuracy, col_name, sentence)
     for key, value in top_dict.items():
-        df.loc[key, 'acc'] = value
-        df.loc[key, 'match-sent'] = sentence
+        if str(df.loc[key, 'acc']) < str(value) or str(df.loc[key, 'acc']) == ''\
+                or str(df.loc[key, 'acc']) == 'nan':
+            df.loc[key, 'acc'] = value
+            df.loc[key, 'match-sent'] = sentence
--- MetchChecker.py
+++ MetchChecker.py
@@ -1,36 +1,39 @@
 import numpy as np
 import pandas as pd
 import difflib
+from fuzzywuzzy import fuzz
 
-
+counter = 0
 def get_score(sentence1, sentence2):
-    # change to " "
-    words1 = sentence1.split(',')
-    words2 = sentence2.split(',')
+    # # change to " "
+    # words1 = sentence1.split(',')
+    # words2 = sentence2.split(',')
+    #
+    # set1 = set(words1)
+    # set2 = set(words2)
+    # if len(set1) > len(set2):
+    #     max_len = len(set1)
+    # else:
+    #     max_len = len(set2)
+    #
+    # inter = set1.intersection(set2)
+    # sum = 0
+    # for word in inter:
+    #     index1 = words1.index(word) + 1
+    #     index2 = words2.index(word) + 1
+    #     if index1 == index2:
+    #         sum += 1
+    #     else:
+    #         sum += 1/np.power(index1, 2) + 1/np.power(index2, 2)
+    #
+    # precent_match = sum/(max_len)
 
-    set1 = set(words1)
-    set2 = set(words2)
-    if len(set1) > len(set2):
-        max_len = len(set1)
-    else:
-        max_len = len(set2)
-
-    inter = set1.intersection(set2)
-    sum = 0
-    for word in inter:
-        index1 = words1.index(word) + 1
-        index2 = words2.index(word) + 1
-        if index1 == index2:
-            sum += 1
-        else:
-            sum += 1/np.power(index1, 2) + 1/np.power(index2, 2)
-
-    precent_match = sum/(max_len)
+    return fuzz.ratio(sentence1, sentence2)
 
 
 
 
-    return precent_match
+    # return precent_match
 
 
 def compare_with_table(data_frame, col_name, sentence):
@@ -43,27 +46,29 @@ def compare_with_table(data_frame, col_name, sentence):
 
 
 def get_top_matches(df, accurecy, col_name, sentence):
+    global counter
     top_dict = {}
     score_list = compare_with_table(df, col_name, sentence)
+    # print (score_list)
     max_item = max(score_list)
     for i in range(len(score_list)):
-        if (max_item - score_list.__getitem__(i)) < accurecy:
-            top_dict[i] = score_list.__getitem__(i)
-
+        if score_list.__getitem__(i) >= 90:
+            if (max_item - score_list.__getitem__(i)) < accurecy:
+                top_dict[i] = score_list.__getitem__(i)
+    print (counter, top_dict)
+    counter +=1
     return top_dict
 
 
-if __name__ == '__main__':
-
-    difflib.get_close_matches("hello i am noa", "hello i an amit")
-
-    sentence = 'Milk, human, mature, fluid'
-    data = {'Food Description':
-                ['Milk, imitation, fluid, non-soy, sweetened, flavors other than chocolate',
-                 'Milk, soy, dry, reconstituted, not babys',
-                 'Yogurt, vanilla, lemon, or coffee flavor, whole milk', 'milk, human']}
-
-    df = pd.DataFrame(data, columns=['Food Description'])
-
-    score_list = get_top_matches(df, 0.5, 'Food Description', sentence)
-    print(score_list)
+# if __name__ == '__main__':
+#
+#     sentence = 'Milk, human, mature, fluid'
+#     data = {'Food Description':
+#                 ['Milk, imitation, fluid, non-soy, sweetened, flavors other than chocolate',
+#                  'Milk, soy, dry, reconstituted, not babys',
+#                  'Yogurt, vanilla, lemon, or coffee flavor, whole milk', 'milk, human']}
+#
+#     df = pd.DataFrame(data, columns=['Food Description'])
+#
+#     score_list = get_top_matches(df, 0.5, 'Food Description', sentence)
+#     print(score_list)
