cd087bf2a1d1a2430237d71cd10017fc9b99d2be
Noa Or <noaor7@gmail.com>
2019-03-12 16:41:02 +0200

    added correlation features, decision tree

--- Correlation/plot_correlation.py
+++ Correlation/plot_correlation.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
    # Compute the correlation matrix
     corr = GI_df.corr()
-
+    corr.to_csv("correlation.csv")
     # Generate a mask for the upper triangle
     mask = np.zeros_like(corr, dtype=np.bool)
     #mask[np.triu_indices_from(mask)] = True
@@ -26,10 +26,21 @@ if __name__ == '__main__':
     # f, ax = plt.subplots(figsize=(11, 9))
 
 
+
     # Generate a custom diverging colormap
-    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    cmap = sns.diverging_palette(200, 10, as_cmap=True)
+
+
+    # sns.clustermap(corr)
+    sns.set(font_scale=0.5)
+    cmn = sns.clustermap(corr, cmap=cmap, col_cluster=False)
+
+    hm = cmn.ax_heatmap.get_position()
+    plt.setp(cmn.ax_heatmap.yaxis.get_majorticklabels(), fontsize=6)
+    cmn.ax_heatmap.set_position([hm.x0, hm.y0, hm.width, hm.height])
+
 
     # Draw the heatmap with the mask and correct aspect ratio
-    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
-                square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    # sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
+    #             square=True, linewidths=.5, cbar_kws={"shrink": .5})
     plt.show()
