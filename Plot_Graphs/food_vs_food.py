6238d4f60f3093a510b9d9c1e33e10a2e7b4f9b5
Amit Hadas <amitr.hadas@gmail.com>
2019-04-14 13:48:04 +0300

    fix food vs food

--- Plot_Graphs/food_vs_food.py
+++ Plot_Graphs/food_vs_food.py
@@ -24,9 +24,14 @@ if __name__ == '__main__':
 
     food_groups = pd.read_excel("GI_USDA_CLEAN_FOOD_GROUPS.xlsx")
     species = food_groups.pop("FdGrp_desc")
-    lut = dict(zip(species.unique(), "rbg"))
+    lut = dict(zip(species.unique(), sns.hls_palette(len(set(species.unique())), l=0.5, s=0.8)))
     row_colors = species.map(lut)
     g = sns.clustermap(distances, row_colors=row_colors, figsize=(13, 13))
+
+    for tick_label in g.ax_heatmap.axes.get_yticklabels():
+        tick_text = tick_label.get_text()
+        species_name = species.loc[int(tick_text)]
+        tick_label.set_color(lut[species_name])
 
     if not os.getcwd().__contains__("Graphs & Photos"):
         os.chdir(os.getcwd()[:os.getcwd().index("Excel_files")] + "Graphs & Photos")
